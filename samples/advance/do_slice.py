8345ad5326fd24cd32302e984831b55fe6323626
burst-bao <53359458+burst-bao@users.noreply.github.com>
2019-08-16 17:15:35 +0800

    Update do_slice.py
    增加了练习题目的答案

--- samples/advance/do_slice.py
+++ samples/advance/do_slice.py
@@ -14,3 +14,22 @@ print('R[-10:] =', R[-10:])
 print('R[10:20] =', R[10:20])
 print('R[:10:2] =', R[:10:2])
 print('R[::5] =', R[::5])
+
+# -*- coding: utf-8 -*-
+def trim(s):
+    l=len(s)
+    while(True):      #先清除掉后面的，再递归，在清除掉前面的
+        if s[-1:]!=' ':
+            s=s[:]
+            break
+        elif s[-1:]==' ':
+            s=s[:l-1]
+            return trim(s)
+    while(True):
+        if s[:1]!=' ':
+            s=s[:]
+            break
+        elif s[:1]==' ':
+            s=s[-l+1:]
+            return trim(s)
+return s
